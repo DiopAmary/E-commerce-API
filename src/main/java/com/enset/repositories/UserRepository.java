7c3e09d2dd903a34f8cfc087ce5d067c358298de
mekouar-mehdi <mehdimekouar92@gmail.com>
2021-04-10 00:50:54 +0100

    adedd user service and its implementation

--- src/main/java/com/enset/repositories/UserRepository.java
+++ src/main/java/com/enset/repositories/UserRepository.java
@@ -1,5 +1,16 @@
 package com.enset.repositories;
 
-public class UserRepository {
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
+import com.enset.entities.UserEntity;
+
+@Repository
+public interface UserRepository extends PagingAndSortingRepository<UserEntity, Long> {
+	public UserEntity findByEmail(String email);
+	public UserEntity findByUserId(String userId);
 }
--- src/main/java/com/enset/services/UserService.java
+++ src/main/java/com/enset/services/UserService.java
@@ -1,5 +1,18 @@
 package com.enset.services;
 
-public class UserService {
+import org.springframework.security.core.userdetails.UserDetailsService;
+import com.enset.dto.UserDto;
 
+public interface UserService extends UserDetailsService {
+	
+	public UserDto createUser(UserDto userDto);
+
+	public UserDto getUser(String email);
+
+	public UserDto getUserByUserId(String UserId);
+
+	public UserDto updateUser(String id, UserDto userDto);
+
+	public void deleteUser(String id);
+	
 }
