7c8e1ec24739adc9577261117d091a64479f235e
Kouhou Mohamed <medko24@gmail.com>
2021-04-19 13:54:38 +0300

    update Livreur

--- src/main/java/com/enset/entities/Livreur.java
+++ src/main/java/com/enset/entities/Livreur.java
@@ -12,15 +12,30 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.FetchType;
 import javax.persistence.OneToMany;
+
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Entity(name = "livreurs")
+@AllArgsConstructor
+@NoArgsConstructor
 public class Livreur{
 	
 	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	
+	private String nom;
+	
+	private String prenom;
+	
+	private String email;
+	
+	private String telephone;
+	
 	private String codeLivreur;
+	
 	
 	@ManyToOne
 	@JoinColumn(name = "address_id", nullable = true)
