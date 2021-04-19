ac8388aa4bae2449748b78664a334570a4641444
Kouhou Mohamed <medko24@gmail.com>
2021-04-19 14:22:26 +0300

    update livreurRepository

--- src/main/java/com/enset/repositories/LivreurRepository.java
+++ src/main/java/com/enset/repositories/LivreurRepository.java
@@ -1,19 +1,21 @@
 package com.enset.repositories;
-
-import java.util.Date;
-
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import com.enset.entities.AddressEntity;
 import com.enset.entities.Livreur;
 
 public interface LivreurRepository extends JpaRepository<Livreur, Long>{
 	
 	public Page<Livreur> findAll(Pageable pageable);
+	
+	public Page<Livreur> findByNom(String nom, Pageable pageable);
+	
+	public Page<Livreur> findByPrenom(String prenom, Pageable pageable);
+	
+	public Livreur findByEmail(String email);
+	
 	public Livreur findByCodeLivreur(String codeLivreur);
+	
 	public Livreur findByAddress(AddressEntity address);
-	public Page<Livreur> findByAddress(AddressEntity address,Pageable pageable);
-
 }
