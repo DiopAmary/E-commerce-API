a2634bdeb61010268583ff420b3083438765972c
DiopAmary <amarydiop27@gmail.com>
2021-04-21 15:44:03 +0000

    CategorieRepository insterface update

--- src/main/java/com/enset/repositories/CategorieRepository.java
+++ src/main/java/com/enset/repositories/CategorieRepository.java
@@ -1,7 +1,6 @@
 package com.enset.repositories;
 
 import com.enset.entities.CategorieEntity;
-import com.enset.entities.ProduitEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CategorieRepository extends JpaRepository<CategorieEntity, Long> {
