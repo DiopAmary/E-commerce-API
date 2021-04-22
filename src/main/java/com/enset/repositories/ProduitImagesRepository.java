06c1502f776d34ac10852e818c0e18651f901371
DiopAmary <amarydiop27@gmail.com>
2021-04-22 20:30:03 +0000

    ProduitImagesRepository update : findByProduit

--- src/main/java/com/enset/repositories/ProduitImagesRepository.java
+++ src/main/java/com/enset/repositories/ProduitImagesRepository.java
@@ -1,7 +1,9 @@
 package com.enset.repositories;
 
+import com.enset.entities.ProduitEntity;
 import com.enset.entities.ProduitImagesEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProduitImagesRepository extends JpaRepository<ProduitImagesEntity, Long> {
+    public ProduitImagesEntity findByProduit(ProduitEntity produit);
 }
